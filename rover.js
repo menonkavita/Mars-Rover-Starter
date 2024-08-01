a235ce65c427670f3fe1b8649aa351c72cf36a2d
K M <menon_kavita@yahoo.com>
2024-08-01 11:50:49 -0500

    Assignment 3: Mars Rover; All 14 tests passing, made updates to code

--- rover.js
+++ rover.js
@@ -23,80 +23,94 @@ class Rover {
       3. Updates certain properties of the rover object.
    */
 
+
+  modeChangeRover(commandValue){
+      let roverProcessCommand = {                              // New Object for processing each Command object
+         completed : false                                     // will have one property - completed
+      };                            
+
+      if (this.mode === commandValue){
+         roverProcessCommand.completed = false;                // 'MODE_CHANGE' unsuccessful, so status set to 'false'.
+      } 
+      else{
+         this.mode = commandValue;
+         roverProcessCommand.completed = true;                 // 'MODE_CHANGE' successful, so status set to 'true'.
+      }
+
+      return roverProcessCommand;
+   }
+
+   statusCheckRover(){
+      let roverProcessCommand = {                              // New Object for processing each Command object; 
+         completed : false                                     // will have 2 properties - completed, roverStatus
+      };  
+
+      let roverStatusProperties = {                            // Object created only when command is 'STATUS_CHECK'; 
+         mode : this.mode,                                     // will get contain current values of 3 properties of Rover.
+         generatorWatts : this.generatorWatts,
+         position : this.position
+      };
+
+      roverProcessCommand.completed = true; 
+      roverProcessCommand.roverStatus = roverStatusProperties;  
+      
+      return roverProcessCommand;
+   }
+
+   moveRover(commandValue){                                    
+      let roverProcessCommand = {                              // New Object for processing each Command object
+         completed : false                                     // will have one property - completed
+      };
+
+      if(this.mode === 'LOW_POWER'){                           // MOVE will happen only when rover has NORMAL power.
+         roverProcessCommand.completed = false;                // If LOW POWER, set status to false.
+      }
+
+      if(this.mode === 'NORMAL'){
+         this.position = commandValue;
+         roverProcessCommand.completed = true;
+      }
+
+      return roverProcessCommand;
+   }
+   
+
    receiveMessage (messageObject){
       let i = 0;
    
       let roverResults = {
          message : messageObject.message,
-         results: []                                                 // roverResults.results [] is an array of objects to store responses
-      };                                                             // to command objects.
+         results: []                                                 // roverResults.results [] is an array of objects to store 
+      };                                                             // responses to command objects.
 
       while (i < messageObject.commands.length){
          let aCommand = messageObject.commands[i];
                   
          if (aCommand.commandType === 'MODE_CHANGE'){
-
-            let roverProcessCommand = {                              // New Object for processing each Command object
-               completed : false
-            };                            
-
-            if (this.mode === aCommand.value){
-               roverProcessCommand.completed = false;                // 'MODE_CHANGE' unsuccessful, so status set to 'false'.
-            } 
-            else{
-               this.mode = aCommand.value;
-               roverProcessCommand.completed = true;                 // 'MODE_CHANGE' successful, so status set to 'true'.
-            }
-
-            roverResults.results.push(roverProcessCommand);          // Pushing status into results array.
+            roverResults.results.push(this.modeChangeRover(aCommand.value));
         }
          
         if(aCommand.commandType === 'STATUS_CHECK'){
-            let roverProcessCommand = {                              // Object created to process each Command object; will have 2 properties - completed, roverStatus
-               completed : false
-            };  
-
-            let roverStatusProperties = {                            // Object created only when command is 'STATUS_CHECK'; Will get contain current values of 3 properties of Rover.
-               mode : this.mode,
-               generatorWatts : this.generatorWatts,
-               position : this.position
-            };
-
-            roverProcessCommand.completed = true; 
-            roverProcessCommand.roverStatus = roverStatusProperties;  
-            roverResults.results.push(roverProcessCommand);          // Pushing task completed status & current rover stats into results array.
+            roverResults.results.push(this.statusCheckRover());
          }
 
-         if(aCommand.commandType === 'MOVE'){                        // MOVE will work only if rover has NORMAL power. Else set status to false.
-            let roverProcessCommand = {                              // New Object for processing each Command object
-               completed : false
-            };
-
-            if(this.mode === 'LOW_POWER'){
-               roverProcessCommand.completed = false;
-            }
-
-            if(this.mode === 'NORMAL'){
-               this.position = aCommand.value;
-               roverProcessCommand.completed = true;
-            }
-            roverResults.results.push(roverProcessCommand);
+         if(aCommand.commandType === 'MOVE'){                        
+            roverResults.results.push(this.moveRover(aCommand.value));
          }
 
          i++;
-      } // end of While
+      }                                                             // end of while
 
       return roverResults;                                          // mesage object should contain message name & results array.
-   } // end of receiveMessage()
-   
-} // end of Rover Class
+   }                                                                // end of receiveMessage()
+}                                                                   // end of Rover Class
 
 
 
-
-let commands = [ new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];   // Move function
+let commands = [ new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];   
 let messageObj = new Message('Test message with Two commands', commands);
-let rover = new Rover(98382);                                                             // Passes 98382 as the rover's position.
+
+let rover = new Rover(98382);                                       // Passes 98382 as the rover's position.
 let response = rover.receiveMessage(messageObj);
 
 console.log(JSON.stringify(response, null, 2));
